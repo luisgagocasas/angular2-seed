5a3a5ce40e10713f15eb7aee4ef8b9677c130181
Luis Gago Casas <luisgago@lagc-peru.com>
2017-08-13 20:33:14 -0500

    bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,13 @@ var webpack = require('webpack');
 var path = require('path');
 var webpackMerge = require('webpack-merge');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ProvidePlugin = require('webpack/lib/ProvidePlugin'); 
 
 // Webpack Config
 var webpackConfig = {
   entry: {
     'main': './src/main.browser.ts',
+    'twbs':  'bootstrap-loader',
   },
 
   output: {
@@ -28,6 +30,25 @@ var webpackConfig = {
       template: 'src/index.html'
     }),
 
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      Tether: "tether",
+      "window.Tether": "tether",
+      Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+      Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
+      Button: "exports-loader?Button!bootstrap/js/dist/button",
+      Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
+      Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
+      Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+      Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
+      Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
+      Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
+      Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
+      Util: "exports-loader?Util!bootstrap/js/dist/util"
+    }),
+
   ],
 
   module: {
@@ -42,7 +63,39 @@ var webpackConfig = {
         ]
       },
       { test: /\.css$/, loaders: ['to-string-loader', 'css-loader'] },
-      { test: /\.html$/, loader: 'raw-loader' }
+      { test: /\.html$/, loader: 'raw-loader' },
+      /*
+      * Sass loader (required for Bootstrap 4)
+      */
+      {
+        test: /\.css$/,
+        use: ['raw-loader']
+      },
+
+      {
+        test: /\.scss$/,
+        use: ['raw-loader', 'sass-loader']
+      },
+
+      /*
+      * Bootstrap 4 loader
+      */
+      {
+        test: /bootstrap\/dist\/js\/umd\//,
+        use: 'imports-loader?jQuery=jquery'
+      },
+
+      /*
+      * Font loaders, required for font-awesome-sass-loader and bootstrap-loader
+      */
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      },
     ]
   }
 
@@ -60,7 +113,7 @@ var defaultConfig = {
   },
 
   resolve: {
-    extensions: [ '.ts', '.js' ],
+    extensions: [ '.ts', '.js', '.css', '.scss' ],
     modules: [ path.resolve(__dirname, 'node_modules') ]
   },
 
